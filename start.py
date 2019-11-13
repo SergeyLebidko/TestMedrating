f9ea84a89a9ed4dc5e570627aeeb7e63011a14e7
SergeyLebidko <it.sergeyler@mail.ru>
2019-11-13 18:55:26 +0300

    Создан код, записывающий в файлы информацию о задачах

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from sys import exit
-from functions import get_data_from_api
+from functions import get_data_from_api, get_user_tasks
 
 users_api_url = 'https://jsonplaceholder.typicode.com/users'
 todos_api_url = 'https://jsonplaceholder.typicode.com/todos'
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     # Получаем данные от api
     try:
         users_data = get_data_from_api(users_api_url)
-        todos_data = get_data_from_api(todos_api_url)
+        tasks_data = get_data_from_api(todos_api_url)
     except Exception as e:
+        # Если возникла ошибка при получении данных от api - выводим сообщение об ошибке и завершаем работу
         print(e)
         input('Для завершения работы нажмите Enter...')
         exit(0)
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     if not os.path.isdir('tasks'):
         os.mkdir('tasks')
 
-    # Во внешнем цикле перебираем пользователей
+    # Во цикле перебираем пользователей
     for user in users_data:
         # Открываем файл, соответствующий пользователю, в режиме "запись"
         filename = 'tasks/' + user['username'] + '.txt'
@@ -32,14 +33,25 @@ if __name__ == '__main__':
         # Готовим вторую строку с названием компании
         company_info = user['company']['name']
 
-        file.write(user_info+'\n')
-        file.write(company_info+'\n')
+        # Записываем в файл данные о пользователе и названии компании
+        file.write(user_info + '\n')
+        file.write(company_info + '\n')
         file.write('\n')
+
+        # Формируем список завешенных пользователем задач и записываем их в файл
         file.write('Завершенные задачи:\n')
-        file.write('--- список завершенных задач\n')
+        completed_tasks = get_user_tasks(tasks_data, user['id'], True)
+        for task in completed_tasks:
+            file.write(task + '\n')
+
+        # Вставляем в файл пустую строку между списками задач
         file.write('\n')
+
+        # Формируем список не завершенных задач и записываем их в файл
         file.write('Оставшиеся задачи:\n')
-        file.write('--- список оставшихся задач\n')
+        not_completed_tasks = get_user_tasks(tasks_data, user['id'], False)
+        for task in not_completed_tasks:
+            file.write(task + '\n')
 
         # Закрываем файл
         file.close()
