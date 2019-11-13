d5833f5e35819372b29988015c25fe96363cb3bc
SergeyLebidko <it.sergeyler@mail.ru>
2019-11-13 17:55:35 +0300

    Добавлена проверка существования папки с отчетами и код ее создания

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import os
 from sys import exit
 from functions import get_data_from_api
 
@@ -5,6 +6,7 @@ users_api_url = 'https://jsonplaceholder.typicode.com/users'
 todos_api_url = 'https://jsonplaceholder.typicode.com/todos'
 
 if __name__ == '__main__':
+    # Получаем данные от api
     try:
         users_data = get_data_from_api(users_api_url)
         todos_data = get_data_from_api(todos_api_url)
@@ -12,3 +14,7 @@ if __name__ == '__main__':
         print(e)
         input('Для завершения работы нажмите Enter...')
         exit(0)
+
+    # Если каталог с отчетами не существует - создаем его
+    if not os.path.isdir('tasks'):
+        os.mkdir('tasks')
