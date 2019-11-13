9f750c5215213368ce04a5066e2ec04693163536
SergeyLebidko <it.sergeyler@mail.ru>
2019-11-13 18:19:58 +0300

    Создан код, записывающий в файлы информацию о пользователях

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from sys import exit
 from functions import get_data_from_api
 
@@ -18,3 +19,27 @@ if __name__ == '__main__':
     # Если каталог с отчетами не существует - создаем его
     if not os.path.isdir('tasks'):
         os.mkdir('tasks')
+
+    # Во внешнем цикле перебираем пользователей
+    for user in users_data:
+        # Открываем файл, соответствующий пользователю, в режиме "запись"
+        filename = 'tasks/' + user['username'] + '.txt'
+        file = open(filename, 'w')
+
+        # Готовим первую строку с информацией о пользователе
+        user_info = user['name'] + '<' + user['email'] + '>' + ' ' + datetime.today().strftime('%d.%m.%Y %H:%M')
+
+        # Готовим вторую строку с названием компании
+        company_info = user['company']['name']
+
+        file.write(user_info+'\n')
+        file.write(company_info+'\n')
+        file.write('\n')
+        file.write('Завершенные задачи:\n')
+        file.write('--- список завершенных задач\n')
+        file.write('\n')
+        file.write('Оставшиеся задачи:\n')
+        file.write('--- список оставшихся задач\n')
+
+        # Закрываем файл
+        file.close()
